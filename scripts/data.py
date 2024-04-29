ec5c8f9a381b95e9bf43ed3eb0a34c4a79d8e524
JulesJ1 <jules.james594@gmail.com>
2024-04-29 22:47:21 +0100

    Model improvements and testing

--- scripts/data.py
+++ scripts/data.py
@@ -5,23 +5,34 @@ from sklearn.model_selection import TimeSeriesSplit
 from entsoe import EntsoePandasClient
 import requests
 
+from astral.sun import sun
+from astral import LocationInfo
+from datetime import datetime,timedelta
 
 
-def energy_api(starttime,endtime,csv = None):
+def energy_api(starttime,endtime = None,csv = None):
     """
     Makes queries to the Entsoe API for the given timespan. 
-    :param starttime: Query start time
-    :param endtime: Query end time
-    :param csv: name of csv file to convert and download dataframe into
-    :return: Pandas dataframe of load and generation data
+
+    Args:
+        starttime: Query start time
+        endtime: Query end time
+        csv: name of csv file to convert and download dataframe into
+    Returns:
+        A pandas dataframe of hourly load and generation data
     """
     
     client = EntsoePandasClient(api_key= "b337a1d6-b64c-49db-ac5a-8a260d29ec52")
     start = pd.Timestamp(starttime, tz = 'Europe/Madrid' )
-    end = pd.Timestamp(endtime, tz = 'Europe/Madrid' )
- 
-    energy = client.query_generation("ES", start = start, end = end)
-    load  = client.query_load("ES", start = start, end=end)
+
+    if endtime:
+        end = pd.Timestamp(endtime, tz = 'Europe/Madrid' )
+        energy = client.query_generation("ES", start = start, end = end)
+        load  = client.query_load("ES", start = start, end=end)
+    else:
+        energy = client.query_generation("ES", start = start)
+        load  = client.query_load("ES", start = start)
+
     df = pd.concat([energy,load],axis=1)
     df["time"] = df.index
     df = df[df['time'].dt.minute == 0]
@@ -34,12 +45,66 @@ def energy_api(starttime,endtime,csv = None):
         df.to_csv(csv, index=True,index_label="time")
     return df
 
-
 def weather_api():
-    r = requests.get()
+    r = requests.get("https://api.openweathermap.org/data/3.0/onecall?lat=40.416775&lon=-3.703790&exclude=current,minutely,daily,alerts&appid=55194385c9175281575e04c124e2fdbc")
+
+    r = r.json()
+
+    df = pd.json_normalize(r["hourly"])
+    df = pd.concat([df["dt"],df["temp"]],axis=1)
+    df["dt"] = pd.to_datetime(df["dt"],unit='s')
+    df = df.set_index('dt').asfreq("1H")
+    
+    return df
+
+
+def weather_data(csv = None,steps = None):
+    """
+
+    
+    
+    """
+    if csv:
+        
+        weather_raw = pd.read_csv("../../weatherhistoric.csv")
+        f = open("../datasets/weather_updated.csv", "w")
+        f.truncate()
+        f.close()
+
+        skiplength = 385
+        freq = steps
+        curr=0
+
+        while(curr < len(weather_raw)):
+            if curr == 0:
+                weather_raw.iloc[curr:curr + freq,:].to_csv("../datasets/weather_updated.csv", mode="a",header = True)   
+            else: 
+                weather_raw.iloc[curr:curr + freq,:].to_csv("../datasets/weather_updated.csv", mode="a",header = False)
+            curr += skiplength * (steps/6)
+            
+
+    weather_df = pd.read_csv("datasets/weather_updated.csv")
+    weather_df = weather_df[["slice dt unixtime","temperature"]]
+    weather_df = weather_df.rename(columns={"slice dt unixtime":"date","temperature":"temp"})
+    weather_df["date"] = pd.to_datetime(weather_df["date"],unit='s')
+    weather_df.set_index("date",inplace =True)
+    
+    return weather_df
 
 
 def load_data(dataset = None,daily = None, start = None, end = None,csv = None):
+    """
+    Loads an energy dataset from a csv or the Entsoe API.
+
+    Args:
+        dataset: If loading from a csv file then this is the file name.
+        daily: Get the daily energy consumption data.
+        start: Start datetime of the data request.
+        end: End datetime of the data request.
+        csv: Name of a csv file if the data should be stored in a file.
+    Returns:
+        A dataframe containing energy consumption and generation data.
+    """
 
     if dataset:
         df = pd.read_csv(dataset)
@@ -50,11 +115,24 @@ def load_data(dataset = None,daily = None, start = None, end = None,csv = None):
     return df
 
 def preprocessing(dataframe,columns = None,daily = None):
+    """
+    Applies the needed preprocessing steps to a dataframe. Removes specified columns,
+    sets the "time" column as the index. Resamples the data as daily if needed. Creates 
+    year, month and week features using the index.
+
+    Args:
+        dataframe: The dataframe to be transformed.
+        columns: Columns to be removed from the dataframe.
+        daily: Resample the data frequency as daily, uses the mean of the hours in each day.
+    Returns:
+        A pandas dataframe transformed using the preprocessing steps.
+    """
     
     df = dataframe
     if columns:
         df = dataframe.drop(columns, axis=1, errors='ignore')
     df["time"] = pd.to_datetime(df["time"], utc = True)
+    df["time"] = df["time"].dt.tz_localize(None)
     if daily:
         return df.resample("D").mean()
     df = df.set_index('time').asfreq("1H")
@@ -66,6 +144,14 @@ def preprocessing(dataframe,columns = None,daily = None):
     return df
 
 def check_missing_values(dataframe):
+    """
+    Checks the dataframe for missing rows in  the index
+
+    Args:
+        dataframe: A dataframe with missing rows.
+    Returns:
+        A pandas dataframe with an hour frequency.
+    """
     
     new_df = pd.DataFrame(
         pd.date_range(
@@ -88,6 +174,22 @@ def check_missing_values(dataframe):
 
 
 def split_data(dataframe,target,train,validation):
+    """
+    Splits a time series dataframe into training, validation and test sets.
+    Each split is in chronological.
+
+    Args:
+        dataframe: A dataframe.
+        target: Column that is being predicted.
+        train: The timestamp for the end of the training set.
+        validation: The timestamp for the end of the validation set.
+    Returns:
+        trainx: Training set features.
+        testx: Test set features.
+        trainy: Training set target.
+        testy: Test set target
+        end_validation: The end of validation set timestamp.
+    """
     df = dataframe
     x = df.drop(target,axis = 1)
     y = df[target]
@@ -98,7 +200,127 @@ def split_data(dataframe,target,train,validation):
     testx,testy  = x.loc[end_validation:, :],y.loc[end_validation:]
     return trainx,testx,trainy,testy, end_validation
 
+
+def fourier_features(feature,cycle_length,order):
+    result = pd.DataFrame()
+
+    k = 2 * np.pi * feature/cycle_length
+    for i in range(1,order+1):
+        result[f"sin_{feature.name}_{i}"] =  np.sin(i*k)
+        result[f"cos_{feature.name}_{i}"]    =  np.cos(i*k)
+    return result
+def tempfeatures(dataframe):
+    temp_features = dataframe["temp"].copy()
+    temp_features = temp_features.to_frame()
+    temp_features['temp_roll_mean_1_day'] = temp_features['temp'].rolling(24, closed='left').mean()
+    temp_features['temp_roll_mean_7_day'] = temp_features['temp'].rolling(24*7, closed='left').mean()
+    temp_features['temp_roll_max_1_day'] = temp_features['temp'].rolling(24, closed='left').max()
+    temp_features['temp_roll_min_1_day'] = temp_features['temp'].rolling(24, closed='left').min()
+    temp_features['temp_roll_max_7_day'] = temp_features['temp'].rolling(24*7, closed='left').max()
+    temp_features['temp_roll_min_7_day'] = temp_features['temp'].rolling(24*7, closed='left').min()
+
+    return temp_features
+
+def create_features(dataframe,weather_df = None):
+    #add weahter_api call for live data
+
+
+    location = LocationInfo(
+    name='Washington DC',
+    region='Spain',
+    timezone='CET')
+
+    calendar_features = pd.DataFrame(index=dataframe.index)
+    calendar_features['month'] = calendar_features.index.month
+    calendar_features['week_of_year'] = calendar_features.index.isocalendar().week
+    calendar_features['week_day'] = calendar_features.index.day_of_week + 1
+    calendar_features['hour_day'] = calendar_features.index.hour + 1
+    sunrise_hour = [
+        sun(location.observer, date=date, tzinfo=location.timezone)['sunrise'].hour
+        for date in dataframe.index
+    ]
+    sunset_hour = [
+        sun(location.observer, date=date, tzinfo=location.timezone)['sunset'].hour
+        for date in dataframe.index
+    ]
+    sun_light_features = pd.DataFrame({
+                            'sunrise_hour': sunrise_hour,
+                            'sunset_hour': sunset_hour}, 
+                            index = dataframe.index
+                        )
+    sun_light_features['daylight_hours'] = (
+        sun_light_features['sunset_hour'] - sun_light_features['sunrise_hour']
+    )
+    sun_light_features['is_daylight'] = np.where(
+                                            (dataframe.index.hour >= sun_light_features['sunrise_hour']) & \
+                                            (dataframe.index.hour < sun_light_features['sunset_hour']),
+                                            1,
+                                            0
+                                        )
+    exo_features = pd.concat([
+                                calendar_features,
+                                sun_light_features,
+                            
+                            ], axis=1)
+
+    month_encoded = fourier_features(exo_features["month"], 12,1)
+    week_of_year_encoded = fourier_features(exo_features['week_of_year'], 52,1)
+    week_day_encoded = fourier_features(exo_features['week_day'], 7,1)
+    hour_day_encoded = fourier_features(exo_features['hour_day'], 24,1)
+    cyclical_features = pd.concat([
+                            month_encoded,
+                            week_of_year_encoded,
+                            week_day_encoded,
+                            hour_day_encoded,
+                        ], axis=1)
+    if weather_df:
+        temp_df = tempfeatures(weather_df)
+        temp_df.index = dataframe.index
+        cyclical_features = pd.concat([cyclical_features,temp_df], axis = 1)
+    #cyclical_features = cyclical_features.join(temp_features["temp"])
+    exo_features = pd.concat([exo_features, cyclical_features], axis=1)
+    return exo_features
 """
+now = datetime.now()
+prevhour = now - timedelta(hours=12)
+starttime = prevhour.strftime("%d/%m/%Y %H:00:00")
+endtime = now.strftime("%Y-%m-%d %H:00:00")
+df =energy_api(starttime,endtime=endtime)
+
+print(df.columns)"""
+
+"""
+df = load_data()
+df = preprocessing(df)
+#temp = tempfeatures(df)
+weather_df = pd.read_csv("datasets/weather_features.csv")
+weather_df = weather_df.loc[weather_df["city_name"] == "Madrid"]
+weather_df = weather_df.drop_duplicates(subset="dt_iso")
+temp = tempfeatures(weather_df)
+print(temp.head)
+print(temp.iloc[70])
+
+temp = temp.dropna()
+print(len(temp))
+exo = create_features(df)
+
+print(len(exo))
+print(exo.iloc[-1])
+exo = exo.dropna()
+print(len(exo))
+"""
+"""
+df = load_data(dataset="datasets/energy_updated.csv")
+df = preprocessing(df)
+t = tempfeatures(weather_data())
+t = t.loc['2024-03-01 00:00:00':'2024-03-01 05:00:00',:] 
+print(t.index)
+print(df.index)
+exo = create_features(df)
+print(exo)"""
+"""
+df = energy_api('2024-03-06 21:00:00','2024-03-06 22:00:00')
+print(df["Actual Load"])
 #"2017-06-30 23:00:00+00:00"
 #'2018-03-31 23:00:00+00:00'
 df = load_data()
--- scripts/xgbmodel.py
+++ scripts/xgbmodel.py
@@ -2,11 +2,13 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.pipeline import Pipeline
 import data 
-from skopt.space import Real, Integer
-from skopt import BayesSearchCV
+#from skopt.space import Real, Integer
+#from skopt import BayesSearchCVh
 import pandas as pd
 from joblib import dump, load
 import numpy as np
+import pickle
+import os
 
 from skforecast.ForecasterBaseline import ForecasterEquivalentDate
 from skforecast.ForecasterAutoreg import ForecasterAutoreg
@@ -14,17 +16,18 @@ from skforecast.model_selection import bayesian_search_forecaster
 from skforecast.model_selection import backtesting_forecaster
 from skforecast.utils import check_y
 
-from astral.sun import sun
-from astral import LocationInfo
+
+from sklearn.preprocessing import PolynomialFeatures
 
 
 class xgboost_model():
-    def __init__(self):
-        self.df = data.load_data()
-        self.trainx,self.testx,self.trainy,self.testy, self.end_validation = data.split_data
+    def __init__(self,dataframe):
+        self.df = dataframe
+        #self.df = data.preprocessing(self.df)
+        #self.trainx,self.testx,self.trainy,self.testy, self.end_validation = data.split_data(self.df,"Actual Load","2023-03-01 23:00:00","2023-06-30 23:00:00")
 
     def split_data(self):
-        self.trainx, self.testx, self.trainy, self.testy = data.split_data(self.df)
+        self.trainx, self.testx, self.trainy, self.testy,self.end_validation = data.split_data(self.df,"total load actual","2023-03-01 23:00:00","2023-06-30 23:00:00")
 
     def build_pipeline(self):
         estimators = [
@@ -33,95 +36,122 @@ class xgboost_model():
 
         return Pipeline(steps = estimators)
 
-    def fourier_features(feature,cycle_length,order):
-        result = pd.DataFrame()
+    def create_features(self,weather_df):
+        
+        self.exo = data.create_features(self.df,weather_df)
+        transformer_poly = PolynomialFeatures(
+                            degree           = 2,
+                            interaction_only = True,
+                            include_bias     = False,
+                            
 
-        k = 2 * np.pi * feature/cycle_length
-        for i in range(1,order+1):
-            result[f"sin_{feature.name}_{i}"] =  np.sin(i*k)
-            result[f"cos_{feature.name}_{i}"]    =  np.cos(i*k)
-        return result
+                        ).set_output(transform="pandas")
+        """    'sin_sunrise_hour_1',
+            'cos_sunrise_hour_1',
+            'sin_sunset_hour_1',
+            'cos_sunset_hour_1',"""
+        poly_cols = [
+            'sin_month_1', 
+            'cos_month_1',
+            'sin_week_of_year_1',
+            'cos_week_of_year_1',
+            'sin_week_day_1',
+            'cos_week_day_1',
+            'sin_hour_day_1',
+            'cos_hour_day_1',
+            'daylight_hours',
+            'is_daylight',
+            'temp_roll_mean_1_day',
+            'temp_roll_mean_7_day',
+            'temp_roll_max_1_day',
+            'temp_roll_min_1_day',
+            'temp_roll_max_7_day',
+            'temp_roll_min_7_day',
+            'temp'
 
-
-    def exo_features(self):
-        location = LocationInfo(
-        name='Washington DC',
-        region='Spain',
-        timezone='CET')
-
-        calendar_features = pd.DataFrame(index=df2.index)
-        calendar_features['month'] = calendar_features.index.month
-        calendar_features['week_of_year'] = calendar_features.index.isocalendar().week
-        calendar_features['week_day'] = calendar_features.index.day_of_week + 1
-        calendar_features['hour_day'] = calendar_features.index.hour + 1
-        sunrise_hour = [
-            sun(location.observer, date=date, tzinfo=location.timezone)['sunrise'].hour
-            for date in df2.index
         ]
-        sunset_hour = [
-            sun(location.observer, date=date, tzinfo=location.timezone)['sunset'].hour
-            for date in df2.index
-        ]
-        sun_light_features = pd.DataFrame({
-                                'sunrise_hour': sunrise_hour,
-                                'sunset_hour': sunset_hour}, 
-                                index = df2.index
-                            )
-        sun_light_features['daylight_hours'] = (
-            sun_light_features['sunset_hour'] - sun_light_features['sunrise_hour']
-        )
-        sun_light_features['is_daylight'] = np.where(
-                                                (df2.index.hour >= sun_light_features['sunrise_hour']) & \
-                                                (df2.index.hour < sun_light_features['sunset_hour']),
-                                                1,
-                                                0
-                                            )
-        exo_features = pd.concat([
-                                    calendar_features,
-                                    sun_light_features,
-                                
-                                ], axis=1)
 
-        month_encoded = fourier_features(exo_features["month"], 12,1)
-        week_of_year_encoded = fourier_features(exo_features['week_of_year'], 52,1)
-        week_day_encoded = fourier_features(exo_features['week_day'], 7,1)
-        hour_day_encoded = fourier_features(exo_features['hour_day'], 24,1)
-        cyclical_features = pd.concat([
-                                month_encoded,
-                                week_of_year_encoded,
-                                week_day_encoded,
-                                hour_day_encoded,
-                            ], axis=1)
-        cyclical_features = pd.concat([cyclical_features,temp_features], axis = 1)
-        #cyclical_features = cyclical_features.join(temp_features["temp"])
-        exo_features = pd.concat([exo_features, cyclical_features], axis=1)
-        print(exo_features.head)
+        poly_features = transformer_poly.fit_transform(self.exo[poly_cols].dropna())
+        poly_features = poly_features.drop(columns=poly_cols)
+        poly_features.columns = [f"poly_{col}" for col in poly_features.columns]
+        poly_features.columns = poly_features.columns.str.replace(" ", "__")
+
+        self.exo = pd.concat([self.exo, poly_features], axis=1)
+        
+        
 
     def search_space(self, trial):
-        search_space  = {
+        self.search_space  = {
             'n_estimators'  : trial.suggest_int('n_estimators', 400, 1200, step=100),
             'max_depth'     : trial.suggest_int('max_depth', 3, 10, step=1),
             'learning_rate' : trial.suggest_float('learning_rate', 0.01, 0.5),
             'reg_alpha'     : trial.suggest_float('reg_alpha', 0, 1, step=0.1),
             'reg_lambda'    : trial.suggest_float('reg_lambda', 0, 1, step=0.1),
         } 
-        return search_space
+        return self.search_space
     
-    def train_model(self):
-        forecast = ForecasterAutoreg(regressor = XGBRegressor(random_state = 1543),lags = 10)
-        forecast.fit(y=trainy)
+    def train(self):
+        self.exo = data.create_features(self.df)
+        self.exo = self.exo.dropna()
+        print(self.exo.head)
+        self.df = pd.concat([self.df["total load actual"],self.exo],axis = 1)
+        #print(self.df.head)
+
+    def train_models(self,weather):
+        
+        #self.create_features()
+
+        self.exo = data.create_features(self.df,weather)
+
+        features = []
+        # Columns that ends with _sin or _cos are selected
+        features.extend(self.exo.filter(regex='^sin_|^cos_').columns.tolist())
+        # columns that start with temp_ are selected
+        features.extend(self.exo.filter(regex='^temp_.*').columns.tolist())
+        # Columns that start with holiday_ are selected
+        features.extend(['temp'])
+        self.exo = self.exo.filter(features, axis=1)
+
+        #removes temp features for testing
+        features = [x for x in features if "temp" not in x]
+        print(features)
+        """
+        self.df = self.df[["total load actual"]].merge(
+                self.exo,
+                left_index=True,
+                right_index=True,
+                how='left'
+            )"""
+
+        self.df = pd.concat([self.df["total load actual"],self.exo],axis = 1)
+        #print(self.exo.index)
+        #self.exo = self.exo.dropna()
+        #print(self.exo.index)
+        
+        self.df = self.df.dropna()
+        self.split_data()
+
+        #print(self.df.iloc[0])
+        print(self.df.head)
+        #print(self.df.index)
+        
+               
+        self.forecast = ForecasterAutoreg(regressor = XGBRegressor(random_state = 1543),lags = 168)
+        self.forecast.fit(y=self.trainy)
+        lags_grid = [[1, 2, 3, 23, 24, 25, 167, 168, 169]]
         results_search, frozen_trial = bayesian_search_forecaster(
-                                        forecaster         = forecast,
+                                        forecaster         = self.forecast,
                                         #y                  = df3.loc[:end_validation, "total actual load"],
                                         #exog               = df3.loc[:end_validation, df3.columns!="total actual load"],
-                                        y                  = temp_features.loc[:end_validation, "total load actual"],
-                                        exog               = temp_features.loc[:end_validation, temp_features.columns!="total load actual"],
-                                        search_space       = search_space,
+                                        y                  = self.df.loc[:self.end_validation, "total load actual"],
+                                        #temp_features.columns!="total load actual"
+                                        exog               = self.df.loc[:self.end_validation, features],
+                                        search_space       = self.search_space,
                                         lags_grid          = lags_grid,
-                                        steps              = 36,
+                                        steps              = 24,
                                         refit              = False,
                                         metric             = 'mean_absolute_error',
-                                        initial_train_size = len(trainy),
+                                        initial_train_size = len(self.trainy),
                                         fixed_train_size   = False,
                                         n_trials           = 20,
                                         random_state       = 123,
@@ -130,11 +160,65 @@ class xgboost_model():
                                         verbose            = False,
                                         show_progress      = True
                                     )
+       
 
 
-        
-model1 = xgboost_model()
-model1.train_model()
+    def backtesting(self):
+        metric, predictions = backtesting_forecaster(
+                          forecaster         = self.forecast,
+                          y                  = self.df["total load actual"],
+                          steps              = 24,
+                          metric             = 'mean_absolute_error',
+                          initial_train_size = len(self.trainy),
+                          refit              = False,
+                          n_jobs             = 'auto',
+                          verbose            = True, # Change to False to see less information
+                          show_progress      = True
+                      )
+        print(metric)
+
+    
+
+    def save_model(self,filename):
+        """        
+        check_file = os.path.isfile(f"models/{filename}")
+        if not check_file:
+            f = open("myfile.txt", "w")"""
+        pickle.dump(self.forecast,open(f"models/{filename}","wb"))
+      
+df = data.load_data(dataset="datasets/energy_updated.csv")    
+df = data.preprocessing(df) 
+
+
+model1 = xgboost_model(df)
+weather_df = data.weather_data()
+weather_df = weather_df.loc[df.index[0]:df.index[-1],:] 
+
+model1.train_models(weather_df)
+
+model1.backtesting()
+
+model1.save_model("xgboost_v1_no_temp.joblib")
+"""
+df2 = data.load_data()
+df2 = data.preprocessing(df2)
+
+model2 = xgboost_model(df2)
+weather_df = pd.read_csv("../datasets/weather_features.csv")
+weather_df = weather_df.loc[weather_df["city_name"] == "Madrid"]
+weather_df = weather_df.drop_duplicates(subset="dt_iso")
+weather_df = weather_df.loc[df2.index[0]:df2.index[-1],:] 
+model2.train_models(weather_df)
+"""
+
+"""
+df_missing = weather_df[weather_df.index.diff()>pd.Timedelta('1H')]
+df_missing['diff'] = weather_df.diff()
+print(df_missing)
+"""
+#model1.create_features(weather_df)
+#print(model1.exo.head)
+#model1.train()
 
 
 
