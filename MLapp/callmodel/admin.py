b3b4141fe2ca7acd5186912f7b3fca8710335fd4
JulesJ1 <jules.james594@gmail.com>
2024-05-09 12:40:02 +0100

    Removed unused Django framework

--- MLapp/callmodel/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Totalload
-
-
-admin.site.register(Totalload)
--- MLapp/callmodel/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from  . import views
-
-urlpatterns = [
-    path("", views.createchart,name="index"),
-    #path("", views.scheduleAPI,name="schedule")
-]
