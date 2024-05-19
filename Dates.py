08ac1ec96a11782f8d2ac5c1df6b47e5b5c6ec7a
JulesJ1 <jules.james594@gmail.com>
2024-05-19 21:16:16 +0100

    Update to xgboost api

--- Dates.py
+++ Dates.py
@@ -1,9 +1,15 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel,ConfigDict
 from datetime import datetime
-from entsoe import EntsoePandasClient
+import pandas as pd
 
 class Dates(BaseModel):
     starttime: datetime = None
     endtime:   datetime = None
-    steps:     int
+    
+
+
+class Predictions(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    Lastwindow: pd.DataFrame
+    steps:  int
--- XGBoostAPI.py
+++ XGBoostAPI.py
@@ -2,12 +2,12 @@ import uvicorn
 from fastapi import FastAPI
 import gunicorn
 import pickle
-from Dates import Dates
+from Dates import Dates,Predictions
 import scripts.data as energy
 import pandas as pd
 from entsoe import EntsoePandasClient
 import os
-
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
@@ -28,14 +28,73 @@ def energydata(data:Dates):
 
     load  = client.query_load("ES", start = starttime, end=endtime)
 
-    load = load.to_dict(orient="index")
+    load.index = pd.to_datetime(load.index)
+    load = load[load.index.minute == 00]
+    load = load.to_dict()
 
 
     return load
 
 @app.post("/predict")
-def predict(data:Dates):
-    pass
+def predict(data:Predictions):
+
+
+
+    lags =  data.Lastwindow
+    lags = pd.DataFrame.from_dict(lags.json())
+    lags.index = pd.to_datetime(lags.index)
+ 
+    if lags.index.tzinfo != None:
+        lags.index.tz_convert(tz = "utc")
+    lags.index = lags.index.tz_localize(None)
+    lags = lags.asfreq("1h")
+    steps = data.steps
+    now = datetime.now()
+
+    with open('models/xgboost_v2_no_temp.joblib', 'rb') as pickle_file:
+        model1 = pickle.load(pickle_file)
+
+    
+    start = now.strftime("%Y-%m-%d %H:00:00")
+    end = now + timedelta(hours=steps)
+    end = end.strftime("%Y-%m-%d %H:00:00")
+    i = pd.date_range(start,end,freq = "1h")
+
+    exo_df = pd.DataFrame(index = i)
+
+    poly_cols =     ['sin_month_1', 
+            'cos_month_1',
+            'sin_week_of_year_1',
+            'cos_week_of_year_1',
+            'sin_week_day_1',
+            'cos_week_day_1',
+            'sin_hour_day_1',
+            'cos_hour_day_1',
+            'daylight_hours',
+            'is_daylight']
+
+    exog = data.create_features(exo_df,poly_cols)
+
+    features = []
+
+    features.extend(exog.filter(regex='^sin_|^cos_').columns.tolist())
+
+    features.extend(exog.filter(regex='^temp_.*').columns.tolist())
+
+    features.extend(['temp'])
+    exog = exog.filter(features, axis=1)
+ 
+    features = [x for x in features if "temp" not in x]
+
+    prediction = model1.predict(
+                steps = steps,
+                last_window = lags["Actual Load"],
+                exog = exog[features]
+            )
+
+    
+    return prediction.to_dict()
+    
 
 if __name__ == "__main__":
     uvicorn.run(app,host="127.0.0.1",port = 8000)
